892a3631f2004d1818515fe1f7ba547b7a9398ee
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-04-15 03:45:44 +0300

    fix: `fancy-link` arrow after content

--- components/fancy-link.vue
+++ components/fancy-link.vue
@@ -12,8 +12,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <NuxtLink :to="props.to" :class="{
-        'relative inline-block transition group text-sky-700 after:content-[\'_↗\']': true,
+    <NuxtLink :to="props.to" class="relative inline-block transition group text-sky-700 after:content-['_↗']" :class="{
         'hover:before:scale-x-100 hover:before:origin-left before:w-full before:h-0.5 \
         before:origin-right before:transition-transform before:duration-500 before:scale-x-0 before:bg-sky-700 \
         before:absolute before:left-0 before:bottom-0': props.underline,
