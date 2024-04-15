28c79ada05ced357f07f6c849fef4cf8fb6cfc15
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-04-15 04:20:46 +0300

    feat: added current interests to research page

--- pages/research.vue
+++ pages/research.vue
@@ -14,7 +14,36 @@ function togglePopup(publication: any) {
 
 <template>
     <main>
-        <div class="flex flex-col gap-16">
+        <div class="flex flex-col gap-12">
+            <div class="flex flex-col gap-6">
+                <h1 class="max-w-2xl text-2xl font-bold tracking-tight sm:text-3xl sm:leading-tight">
+                    Current interests
+                </h1>
+                <div class="flex flex-col gap-2">
+                    <p>I am currently working on my <strong>Master's thesis</strong>, exploring how large foundation
+                        models can be used
+                        to
+                        solve <strong>multi-step reasoning problems via task decomposition</strong>.
+                        My <strong>interests</strong> at the moment include:</p>
+                    <ul class="list-disc pl-6">
+                        <li><strong>Knowledge representation</strong> in deep models (e.g., embeddings), particularly
+                            using formal
+                            languages.
+                        </li>
+                        <li>Deep architectures and methods for solving complex <strong>reasoning problems</strong>
+                            (e.g., mathematics or logic).
+                        </li>
+                        <li><strong>Reinforcement and imitation learning</strong>, particularly meta-learning the RL/IL
+                            process.</li>
+                    </ul>
+                    <p>My <strong>long term ambition</strong> is to design models capable of not just imitating training
+                        data, but of discovering and displaying new knowledge and behavior. Today, even the largest
+                        models
+                        available struggle
+                        to capture and extrapolate complex reasoning patterns in data in order to exhibit valuable novel
+                        behavior.</p>
+                </div>
+            </div>
             <div class="flex flex-col gap-6">
                 <h1 class="max-w-2xl text-2xl font-bold tracking-tight sm:text-3xl sm:leading-tight">
                     Publications
