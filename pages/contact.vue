8b0a20ed1b07346e3098d8b3e7ec87a23cd6eb42
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-09-01 19:55:49 +0200

    fix: typo in contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,7 +10,7 @@ const DOT = '|DOT|';
                 How to contact me
             </h1>
             <p>
-                If you would like to <strong>collaborate</strong>, <strong>discuss opportunities</strong>, or <strong>ask any question</strong>, please get in touch! I am be very happy to address these.
+                If you would like to <strong>collaborate</strong>, <strong>discuss opportunities</strong>, or <strong>ask any question</strong>, please get in touch! I am very happy to address these.
             </p>
             <p>
                 The best way to contact me is by sending an <strong>email to my address</strong> <span class="code">contact{{ DOT }}sergiohernandez{{ AT }}gmail{{ DOT }}com</span> (replace the <span class="code">{{ DOT }}</span> and <span class="code">{{ AT }}</span>; I'm trying to avoid email scrapers). I do not actively use social networks.
